df9b0bd63979615bb3908ea0a33e48d64fbed1f7
Sławomir Wosek <swosek@im.umg.edu.pl>
2024-04-09 12:51:32 +0200

    chore: project setup with spring module

---
+++ penguins-service/src/main/java/pl/swosek/sample/penguinsservice/PenguinsServiceApplication.java
@@ -0,0 +1,13 @@
+package pl.swosek.sample.penguinsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PenguinsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PenguinsServiceApplication.class, args);
+    }
+
+}
---
+++ penguins-service/src/test/java/pl/swosek/sample/penguinsservice/PenguinsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.swosek.sample.penguinsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PenguinsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
