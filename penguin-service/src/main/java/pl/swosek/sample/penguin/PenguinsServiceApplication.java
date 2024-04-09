4c68bc80dd5871787ca93cbcf1b428c467f77a61
Sławomir Wosek <swosek@im.umg.edu.pl>
2024-04-09 14:30:36 +0200

    chore: changing names

--- penguin-service/src/main/java/pl/swosek/sample/penguin/PenguinsServiceApplication.java
+++ penguin-service/src/main/java/pl/swosek/sample/penguin/PenguinsServiceApplication.java
@@ -1,4 +1,4 @@
-package pl.swosek.sample.penguinsservice;
+package pl.swosek.sample.penguin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- penguin-service/src/test/java/pl/swosek/sample/penguin/PenguinsServiceApplicationTests.java
+++ penguin-service/src/test/java/pl/swosek/sample/penguin/PenguinsServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package pl.swosek.sample.penguinsservice;
+package pl.swosek.sample.penguin;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
