4d89d4e306afd6e626c750f5785cd4796638d32a
Sławomir Wosek <swosek@im.umg.edu.pl>
2024-04-12 12:09:25 +0200

    refactor: main component divided into navigation and main component and cdkTable implementation

--- penguin-ng/src/app/app.module.ts
+++ penguin-ng/src/app/app.module.ts
@@ -6,6 +6,22 @@ import { AppComponent } from './app.component';
 import { PenguinTableViewComponent } from './penguin/view/penguin-table-view/penguin-table-view.component';
 import { HttpClientModule } from "@angular/common/http";
 import { MatTableModule } from "@angular/material/table";
+import { MainComponent } from './layout/main/main.component';
+import { MainNavigationComponent } from './layout/main-navigation/main-navigation.component';
+import { MatButtonModule } from "@angular/material/button";
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import {
+  CdkCell,
+  CdkCellDef,
+  CdkColumnDef,
+  CdkHeaderCell,
+  CdkHeaderCellDef, CdkHeaderRow,
+  CdkHeaderRowDef, CdkRow,
+  CdkRowDef, CdkTable
+} from "@angular/cdk/table";
 
 /**
  * Application main module.
@@ -13,15 +29,34 @@ import { MatTableModule } from "@angular/material/table";
 @NgModule({
   declarations: [
     AppComponent,
+    MainComponent,
+    MainNavigationComponent,
     PenguinTableViewComponent
   ],
   imports: [
     AppRoutingModule,
     BrowserModule,
     HttpClientModule,
-    MatTableModule
+    MatTableModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    CdkHeaderCell,
+    CdkCell,
+    CdkColumnDef,
+    CdkHeaderRowDef,
+    CdkRowDef,
+    CdkCellDef,
+    CdkHeaderCellDef,
+    CdkHeaderRow,
+    CdkRow,
+    CdkTable
   ],
-  providers: [],
+  providers: [
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
