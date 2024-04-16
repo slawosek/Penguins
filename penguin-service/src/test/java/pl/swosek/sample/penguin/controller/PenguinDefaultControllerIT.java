baa4d6fe6bde8de7ad6770d2c18a716edeffa4a9
Sławomir Wosek <swosek@im.umg.edu.pl>
2024-04-16 12:26:44 +0200

    test: integration test for put method

--- penguin-service/src/test/java/pl/swosek/sample/penguin/controller/PenguinDefaultControllerIT.java
+++ penguin-service/src/test/java/pl/swosek/sample/penguin/controller/PenguinDefaultControllerIT.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.web.server.ResponseStatusException;
 import pl.swosek.sample.penguin.data.controller.dto.GetPenguinResponse;
 import pl.swosek.sample.penguin.data.controller.dto.GetPenguinsResponse;
+import pl.swosek.sample.penguin.data.controller.dto.PutPenguinRequest;
 import pl.swosek.sample.penguin.data.controller.function.PenguinToResponseFunction;
 import pl.swosek.sample.penguin.data.controller.function.PenguinsToResponseFunction;
 import pl.swosek.sample.penguin.data.controller.impl.PenguinDefaultController;
@@ -103,6 +104,28 @@ public class PenguinDefaultControllerIT {
         String actual  = assertThrows(ResponseStatusException.class,
                 () -> controller.getPenguin("999999"))
                 .getMessage();
+
+        assertThat(expected).isEqualTo(actual);
+    }
+
+    @Test
+    void putChangedPenguin() {
+        Penguin expected = Penguin.builder()
+                .taxonKey("2481660")
+                .acceptedScientificName("Better name")
+                .species("Penguinis cuties")
+                .scientificName("Aptenodytes patagonicus J.F.Miller 1778")
+                .numberOfOccurrences(150)
+                .build();
+        controller.putPenguin("2481660",
+                PutPenguinRequest.builder()
+                        .acceptedScientificName("Better name")
+                        .species("Penguinis cuties")
+                        .scientificName("Aptenodytes patagonicus J.F.Miller 1778")
+                        .numberOfOccurrences(150)
+                        .build());
+
+        Penguin actual = repository.findById("2481660").get();
 
         assertThat(expected).isEqualTo(actual);
     }
